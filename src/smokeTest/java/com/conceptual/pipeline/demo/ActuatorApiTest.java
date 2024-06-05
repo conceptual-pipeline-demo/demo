0533023a5c6c22b54d98052126235623c02e9649
binfanghuang <bfhuang@thoughtworks.com>
2024-06-05 15:13:59 +0800

    [Tech][Binfang]add smoke test

---
+++ src/smokeTest/java/com/conceptual/pipeline/demo/ActuatorApiTest.java
@@ -0,0 +1,19 @@
+package com.conceptual.pipeline.demo;
+
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+
+class ActuatorApiTest {
+
+    @Test
+    void test_actuator_health_api() throws Exception {
+        RestAssured.given()
+                .get("http://localhost:9090/actuator/health")
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .body("status", Matchers.equalTo("UP"));
+    }
+}
